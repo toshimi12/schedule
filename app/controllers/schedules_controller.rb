02ea34fd17bb44ebc6a0a6c4b5bfbb18bba5b93e
toshimi12 <funyutoshimi@gmail.com>
2023-02-21 00:10:16 +0000

    rails newコマンドを実行し、アプリケーションを作成

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -17,14 +17,27 @@ class SchedulesController < ApplicationController
   end
   
   def show
+    @schedule = Schedule.find(params[:id])
   end
   
   def edit
+    @schedule = Schedule.find(params[:id])
   end
   
   def update
+    @schedule = Schedule.find(params[:id])
+    if @schedule.update(params.require(:schedule).permit(:title, :start_at, :end_at, :allday, :memo))
+       flash[:notice] = "ユーザーIDが「#{@schedule.id}」の情報を更新しました"
+       redirect_to :schedules
+    else
+       render "edit"
+    end
   end
   
   def destroy
+    @schedule = Schedule.find(params[:id])
+    @schedule.destroy
+     flash[:notice] = "ユーザーを削除しました"
+     redirect_to :schedules
   end
 end
--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -1,2 +1,3 @@
-Date::DATE_FORMATS[:date_jp] = "%Y年%m月%d日"
-Date::DATE_FORMATS[:datetime_jp] ="%Y年%m月%d日%H時%M分"
+Time::DATE_FORMATS[:datetime_jp] = '%Y年 %m月 %d日 %H時 %M分'
+Time::DATE_FORMATS[:date] = '%Y/%m/%d'
+Time::DATE_FORMATS[:datetime_jp] = '%Y%m%d'
